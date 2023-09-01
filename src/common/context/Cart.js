eb8c4d174250babbaff5bea5bdbcbddb2a89cfdb
victorcabral182 <107046713+victorcabral182@users.noreply.github.com>
2023-08-31 22:01:18 -0300

    Conclusão

--- src/common/context/Cart.js
+++ src/common/context/Cart.js
@@ -1,74 +1,109 @@
-import { useContext, useEffect, useState, createContext } from "react"
+import { useContext, useEffect, useState, createContext } from "react";
+import { usePaymentContext } from "./Payment";
+import { UserContext } from "./User";
 
-export const CartContext = createContext()
+export const CartContext = createContext();
 
-CartContext.displayName = "Cart"
+CartContext.displayName = "Cart";
 
 export const CartProvider = ({ children }) => {
-  const [cart, setCart] = useState([])
-  const [quantityProduct, setQuantityProduct] = useState(0)
+  const [cart, setCart] = useState([]);
+  const [quantityProduct, setQuantityProduct] = useState(0);
+  const [valorTotalCarrinho, setValorTotalCarrinho] = useState(0);
+
   return (
     <CartContext.Provider
-      value={{ cart, setCart, quantityProduct, setQuantityProduct }}
+      value={{
+        cart,
+        setCart,
+        quantityProduct,
+        setQuantityProduct,
+        valorTotalCarrinho,
+        setValorTotalCarrinho,
+      }}
     >
       {children}
     </CartContext.Provider>
-  )
-}
+  );
+};
 
 export const useCartContext = () => {
-  const { cart, setCart, quantityProduct, setQuantityProduct } =
-    useContext(CartContext)
+  const {
+    cart,
+    setCart,
+    quantityProduct,
+    setQuantityProduct,
+    valorTotalCarrinho,
+    setValorTotalCarrinho,
+  } = useContext(CartContext);
+
+  const { formaPagamento } = usePaymentContext();
+  const { setSaldo } = useContext(UserContext);
+
+  function efetuarCompra() {
+    setCart([]);
+    setSaldo((saldoAtual) => saldoAtual - valorTotalCarrinho);
+  }
 
   function removeFromCart(id) {
-    const product = cart.find((itemInCart) => itemInCart.id === id)
-    const isLastProduct = product.quantidade === 1
+    const product = cart.find((itemInCart) => itemInCart.id === id);
+    const isLastProduct = product.quantidade === 1;
     if (isLastProduct) {
       return setCart((prevCart) =>
         prevCart.filter((itemInCart) => itemInCart.id !== id)
-      )
+      );
     }
     setCart((prevCart) =>
       prevCart.map((itemInCart) => {
         if (itemInCart.id === id) {
-          itemInCart.quantidade -= 1
+          itemInCart.quantidade -= 1;
         }
-        return itemInCart
+        return itemInCart;
       })
-    )
+    );
   }
 
   function addToCart(newProduct) {
     const hasProduct = cart.some(
       (itemInCart) => itemInCart.id === newProduct.id
-    )
+    );
     if (!hasProduct) {
-      newProduct.quantidade = 1
-      return setCart((prevCart) => [...prevCart, newProduct])
+      newProduct.quantidade = 1;
+      return setCart((prevCart) => [...prevCart, newProduct]);
     }
     return setCart((prevCart) =>
       prevCart.map((itemInCart) => {
         if (itemInCart.id === newProduct.id) {
-          itemInCart.quantidade += 1
+          itemInCart.quantidade += 1;
         }
-        return itemInCart
+        return itemInCart;
       })
-    )
+    );
   }
 
   useEffect(() => {
-    const newQuantity = cart.reduce(
-      (counter, product) => counter + product.quantidade,
-      0
-    )
-    setQuantityProduct(newQuantity)
-  }, [cart, setQuantityProduct])
-
+    const { newTotal, newQuantity } = cart.reduce(
+      (counter, product) => ({
+        newQuantity: counter + product.quantidade,
+        newTotal: counter.newTotal + product.valor * product.quantidade,
+      }),
+      {
+        newQuantity: 0,
+        newTotal: 0,
+      }
+    );
+    setQuantityProduct(newQuantity);
+    setValorTotalCarrinho(newTotal * formaPagamento.juros);
+  }, [cart, setQuantityProduct, setValorTotalCarrinho, formaPagamento]);
   return {
     cart,
     setCart,
     addToCart,
     removeFromCart,
     quantityProduct,
-  }
-}
+    setQuantityProduct,
+    valorTotalCarrinho,
+    setValorTotalCarrinho,
+    efetuarCompra,
+  };
+};
--- src/pages/Carrinho/index.js
+++ src/pages/Carrinho/index.js
@@ -6,7 +6,7 @@ import {
   MenuItem,
 } from "@material-ui/core";
 import MuiAlert from "@material-ui/lab/Alert";
-import { useState } from "react";
+import { useContext, useEffect, useMemo, useState } from "react";
 import {
   Container,
   Voltar,
@@ -17,14 +17,21 @@ import { useCartContext } from "common/context/Cart";
 import Produto from "components/Produto";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import { usePaymentContext } from "common/context/Payment";
+import { UserContext } from "common/context/User";
 
 function Carrinho() {
   const history = useHistory();
   const [openSnackbar, setOpenSnackbar] = useState(false);
 
-  const { cart } = useCartContext();
+  const { cart, valorTotalCarrinho, efetuarCompra } = useCartContext();
+  const { saldo = 0 } = useContext(UserContext);
   const { formaPagamento, mudarFormaPagamento, PaymentTypes } =
     usePaymentContext();
+
+  const total = useMemo(
+    () => saldo - valorTotalCarrinho,
+    [saldo, valorTotalCarrinho]
+  );
 
   return (
     <Container>
@@ -49,23 +56,25 @@ function Carrinho() {
       <TotalContainer>
         <div>
           <h2>Total no Carrinho: </h2>
-          <span>R$ </span>
+          <span>R$ {valorTotalCarrinho.toFixed(2)}</span>
         </div>
         <div>
           <h2> Saldo: </h2>
-          <span> R$ </span>
+          <span> R$ {Number(saldo).toFixed(2)}</span>
         </div>
         <div>
           <h2> Saldo Total: </h2>
-          <span> R$ </span>
+          <span> R$ {total.toFixed(2)}</span>
         </div>
       </TotalContainer>
       <Button
         onClick={() => {
+          efetuarCompra();
           setOpenSnackbar(true);
         }}
         color="primary"
         variant="contained"
+        disabled={total < 0 || cart.length === 0}
       >
         Comprar
       </Button>
